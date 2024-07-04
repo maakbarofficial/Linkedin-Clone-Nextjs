d7a00eafdbfcbb17f8e485c67b450ba54cb4c3bb
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-04 21:15:22 +0500

    User Information Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@/components/ui/button";
+import UserInformation from "@/components/UserInformation";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>Linkedin Clone</h1>
-      <Button>Click Me</Button>
+      <section>
+        {/* User Information */}
+        <UserInformation />
+      </section>
+
+      <section>
+        {/* Post Form */}
+        {/* Post Feed */}
+      </section>
+
+      <section>{/* Widget */}</section>
     </main>
   );
 }
